1dc86e93adbbc21aa5e65988e1aa6ce820e8ba5a
Tahmed15 <78212428+Tahmed15@users.noreply.github.com>
2021-03-14 16:07:20 -0400

    Update script.js

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -10,7 +10,12 @@ let position = 0;
 
 container.querySelector('.prev').onclick = function() {
     position += width*count;
-}
+    position = Math.min(position, 0)
+    list.style.marginLeft = position + 'px';
+};
+
 container.querySelector('.next').onclick = function() {
     position -= width * count;
+    position = Math.max(position, -width *(listElems.length - count));
+    list.style.marginLeft = position +'px';
 };
