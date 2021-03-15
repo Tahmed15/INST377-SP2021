1758d9abfc3dea1419c8e361746aaae98a7129f9
Tahmed15 <78212428+Tahmed15@users.noreply.github.com>
2021-03-15 13:57:26 -0400

    cleanup

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,23 +1,6 @@
-// const { event } = require("cypress/types/jquery");
+const { event } = require("cypress/types/jquery");
 
 // /* Put your javascript in here */
-// function clickToChangeBox() {
-//     const prev = document.querySelector('prev');
-//     const next = document.querySelector('next');
-
-//     prev.addEventListener('click', (event)=> {
-//         console.log('clicked prev');
-
-
-//         //const box = document.querySelector('#box1');
-//         //const classes = box.classList;
-//         //console.log(classes.value);
-//         //const newClass = (classes.value === "main") ? "second" : "main";
-//         //console.log(newClass);
-//         //box.classList = newClass;
-//     });
-// }
-// window.onload = clickToChangeBox;
 
 let i = 1;
     for(let li of carousel.querySelectorAll('li')) {
@@ -26,19 +9,17 @@ let i = 1;
       i++;
     }
 
-    /* configuration */
-    let width = 130; // image width
-    let count = 3; // visible images count
+    let width = 130; 
+    let count = 3; 
 
     let list = carousel.querySelector('ul');
     let listElems = carousel.querySelectorAll('li');
 
-    let position = 0; // ribbon scroll position
+    let position = 0; 
 
     carousel.querySelector('.prev').onclick = function() {
       // shift left
       position += width * count;
-      // can't move to the left too much, end of images
       position = Math.min(position, 0)
       list.style.marginLeft = position + 'px';
     };
@@ -46,7 +27,6 @@ let i = 1;
     carousel.querySelector('.next').onclick = function() {
       // shift right
       position -= width * count;
-      // can only shift the ribbbon for (total ribbon length - visible count) images
       position = Math.max(position, -width * (listElems.length - count));
       list.style.marginLeft = position + 'px';
     };
