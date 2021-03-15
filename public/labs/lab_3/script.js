9a6edf949355e1cd5c8e2eb658030fcfe347c315
Tahmed15 <78212428+Tahmed15@users.noreply.github.com>
2021-03-15 13:20:40 -0400

    Update script.js

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,23 +1,14 @@
 const { event } = require("cypress/types/jquery");
 
 /* Put your javascript in here */
-
-
-let width = 140;
-let count = 3;
-
-let list = container.querySelector('ul');
-let listElems = container.querySelectorAll('li');
-let position = 0;
-
-container.querySelector('.prev').onclick = function() {
-    position += width*count;
-    position = Math.min(position, 0)
-    list.style.marginLeft = position + 'px';
-};
-
-container.querySelector('.next').onclick = function() {
-    position -= width * count;
-    position = Math.max(position, -width *(listElems.length - count));
-    list.style.marginLeft = position +'px';
-};
+function clickToChangeBox() {
+    document.addEventListener('click', (event)=> {
+        const box = document.querySelector('#box1');
+        const classes = box.classList;
+        console.log(classes.value);
+        const newClass = (classes.value === "main") ? "second" : "main";
+        console.log(newClass);
+        box.classList = newClass;
+    });
+}
+window.onload = clickToChangeBox;
