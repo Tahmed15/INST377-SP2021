0dfd893acdbc94e67b98ccaf7b00617f26d21074
Tahmed15 <78212428+Tahmed15@users.noreply.github.com>
2021-03-15 13:38:13 -0400

    div

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -2,13 +2,19 @@ const { event } = require("cypress/types/jquery");
 
 /* Put your javascript in here */
 function clickToChangeBox() {
-    document.addEventListener('click', (event)=> {
-        const box = document.querySelector('#box1');
-        const classes = box.classList;
-        console.log(classes.value);
-        const newClass = (classes.value === "main") ? "second" : "main";
-        console.log(newClass);
-        box.classList = newClass;
+    const prev = document.querySelector('prev');
+    const next = document.querySelector('next');
+
+    prev.addEventListener('click', (event)=> {
+        console.log('clicked prev');
+
+
+        //const box = document.querySelector('#box1');
+        //const classes = box.classList;
+        //console.log(classes.value);
+        //const newClass = (classes.value === "main") ? "second" : "main";
+        //console.log(newClass);
+        //box.classList = newClass;
     });
 }
 window.onload = clickToChangeBox;
