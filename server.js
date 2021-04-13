ed8061d0db37a9ff8637f231d1d54045df05bbfe
Tahmed15 <78212428+Tahmed15@users.noreply.github.com>
2021-04-13 14:34:18 -0400

    Update server.js

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.use(connectReload());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(staticFolder));
+app.use(express.static(staticFolder));
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
